9df4c16e99e6ab8a0c7214a278900cae085f3748
Jon Spencer <jon@jonspencer.ca>
2013-08-18 23:18:48 -0700

    simple test case for the png decoder

---
+++ test/png_codec.js
@@ -0,0 +1,57 @@
+
+var fs = require('fs');
+var path = require('path');
+var assert = require('assert');
+var picha = require('../index.js');
+
+describe('png_codec', function() {
+	var asyncPng, syncPng;
+	var asyncImage, syncImage;
+	describe("decode", function() {
+		var file;
+		it("should load test png", function(done) {
+			fs.readFile(path.join(__dirname, "test.png"), function(err, buf) {
+				file = buf;
+				done(err);
+			});
+		});
+		it("should async decode", function(done) {
+			picha.decodePng(file, function(err, image) {
+				asyncImage = image;
+				done(err);
+			});
+		});
+		it("should sync decode", function() {
+			syncImage = picha.decodePngSync(file);
+		});
+		it("should be the same sync or async", function() {
+			assert(syncImage.equalPixels(asyncImage));
+		})
+	})
+	describe("encode", function() {
+		it("should async encode", function(done) {
+			picha.encodePng(asyncImage, function(err, blob) {
+				asyncPng = blob;
+				done(err);
+			})
+		});
+		it("should sync encode", function() {
+			syncPng = picha.encodePngSync(syncImage);
+		})
+		it("should be the same sync or async", function() {
+			assert(asyncPng.compare(syncPng) == 0);
+		})
+	})
+	describe("round trip", function() {
+		it("async match original", function(done) {
+			picha.decodePng(asyncPng, function(err, image) {
+				assert(image.equalPixels(asyncImage));
+				done(err);
+			});
+		})
+		it("sync match original", function() {
+			image = picha.decodePngSync(syncPng);
+			assert(image.equalPixels(syncImage));
+		})
+	})
+})
