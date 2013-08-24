daf21d387373731db3d1ecb9697c63bf9344de07
Jon Spencer <jon@jonspencer.ca>
2013-08-23 22:38:29 -0700

    avoid copying pixels decoding png's

--- src/pngcodec.cc
+++ src/pngcodec.cc
@@ -82,23 +82,6 @@ namespace picha {
 	//----------------------------------------------------------------------------------------------------------------
 	//--
 
-	struct PngCtx {
-		char *error;
-		PngCtx() : error(0) {}
-
-		static void onError(png_structp png_ptr, png_const_charp error) {
-			PngCtx * self = (PngCtx*)png_get_error_ptr(png_ptr);
-			self->error = strdup(error);
-			longjmp(png_jmpbuf(png_ptr), 1);
-		}
-
-		static void onWarn(png_structp png_ptr, png_const_charp error) {}
-	};
-
-
-	//----------------------------------------------------------------------------------------------------------------
-	//--
-
 	struct ReadBuffer {
 		char * srcdata;
 		size_t srclen;
@@ -116,38 +99,75 @@ namespace picha {
 		ctx->srclen -= length;
 	}
 
-	struct PngDecodeCtx : PngCtx {
-		Persistent<Object> buffer;
-		Persistent<Function> cb;
-
+	struct PngReader {
+		png_infop info_ptr;
+		png_structp png_ptr;
 		ReadBuffer readbuf;
-		PixelMode pixel;
+		char * error;
 
-		NativeImage image;
+		PngReader() : info_ptr(0), png_ptr(0), error(0) {}
 
-		void doWork();
+		~PngReader() {
+			if (png_ptr)
+				png_destroy_read_struct(&png_ptr, &info_ptr, 0);
+			if (error)
+				free(error);
+		}
+
+		void close() {
+			if (png_ptr)
+				png_destroy_read_struct(&png_ptr, &info_ptr, 0);
+			info_ptr = 0;
+			png_ptr = 0;
+		}
+
+		void open(char * data, size_t len);
+
+		void decode(const NativeImage &dst);
+
+		int width() { return png_get_image_width(png_ptr, info_ptr); }
+
+		int height() { return png_get_image_height(png_ptr, info_ptr); }
+
+		PixelMode pixel() {
+			png_byte color = png_get_color_type(png_ptr, info_ptr);
+			if ((color & (PNG_COLOR_MASK_COLOR | PNG_COLOR_MASK_PALETTE)) && (color & PNG_COLOR_MASK_ALPHA))
+				return RGBA_PIXEL;
+			else if ((color & (PNG_COLOR_MASK_COLOR | PNG_COLOR_MASK_PALETTE)))
+				return RGB_PIXEL;
+			else if ((color & PNG_COLOR_MASK_ALPHA))
+				return GREYA_PIXEL;
+			else
+				return GREY_PIXEL;
+		}
+
+		static void onError(png_structp png_ptr, png_const_charp error) {
+			PngReader * self = (PngReader*)png_get_error_ptr(png_ptr);
+			self->error = strdup(error);
+			longjmp(png_jmpbuf(png_ptr), 1);
+		}
+
+		static void onWarn(png_structp png_ptr, png_const_charp error) {}
 	};
 
-	void PngDecodeCtx::doWork() {
+	void PngReader::open(char * buf, size_t len) {
+		readbuf.srclen = len;
+		readbuf.srcdata = buf;
+
 		if (png_sig_cmp((png_bytep)readbuf.srcdata, 0, readbuf.srclen < 8 ? readbuf.srclen : 8) != 0) {
 			error = strdup("png signature mismatch");
 			return;
 		}
 
-		png_infop info_ptr = 0;
-		png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, 0, 0, 0);
+		png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, 0, 0, 0);
 		if (png_ptr == 0) {
 			error = strdup("failed to initialize png reader");
 			return;
 		}
 
-		png_bytep * rows = 0;
-		png_set_error_fn(png_ptr, this, PngCtx::onError, PngCtx::onWarn);
-		if (setjmp(png_jmpbuf(png_ptr))) {
-			png_destroy_read_struct(&png_ptr, &info_ptr, 0);
-			delete[] rows;
+		png_set_error_fn(png_ptr, this, PngReader::onError, PngReader::onWarn);
+		if (setjmp(png_jmpbuf(png_ptr)))
 			return;
-		}
 
 		info_ptr = png_create_info_struct(png_ptr);
 		if (info_ptr == 0) {
@@ -156,39 +176,40 @@ namespace picha {
 		}
 
 		png_set_read_fn(png_ptr, &readbuf, do_read_record);
-		png_read_info(png_ptr, info_ptr);
 
-		if (pixel < 0 || pixel >= NUM_PIXELS) {
-			png_byte color = png_get_color_type(png_ptr, info_ptr);
-			if ((color & (PNG_COLOR_MASK_COLOR | PNG_COLOR_MASK_PALETTE)) && (color & PNG_COLOR_MASK_ALPHA))
-				pixel = RGBA_PIXEL;
-			else if ((color & (PNG_COLOR_MASK_COLOR | PNG_COLOR_MASK_PALETTE)))
-				pixel = RGB_PIXEL;
-			else if ((color & PNG_COLOR_MASK_ALPHA))
-				pixel = GREYA_PIXEL;
-			else
-				pixel = GREY_PIXEL;
+		png_read_info(png_ptr, info_ptr);
+	}
+
+	void PngReader::decode(const NativeImage &dst) {
+		assert(dst.width == width());
+		assert(dst.height == height());
+
+		png_bytep * rows = 0;
+		png_set_error_fn(png_ptr, this, PngReader::onError, PngReader::onWarn);
+		if (setjmp(png_jmpbuf(png_ptr))) {
+			delete[] rows;
+			return;
 		}
 
-		if (pixel == RGB_PIXEL) {
+		if (dst.pixel == RGB_PIXEL) {
 			png_set_gray_to_rgb(png_ptr);
 			png_set_palette_to_rgb(png_ptr);
 			png_set_expand(png_ptr);
 			png_set_strip_alpha(png_ptr);
 		}
-		else if (pixel == RGBA_PIXEL) {
+		else if (dst.pixel == RGBA_PIXEL) {
 			png_set_gray_to_rgb(png_ptr);
 			png_set_palette_to_rgb(png_ptr);
 			png_set_expand(png_ptr);
 			png_set_tRNS_to_alpha(png_ptr);
 			png_set_add_alpha(png_ptr, ~0, PNG_FILLER_AFTER);
 		}
-		else if (pixel == GREY_PIXEL) {
+		else if (dst.pixel == GREY_PIXEL) {
 			png_set_strip_alpha(png_ptr);
 			png_set_rgb_to_gray(png_ptr, 1, -1, -1);
 			png_set_expand_gray_1_2_4_to_8(png_ptr);
 		}
-		else if (pixel == GREYA_PIXEL) {
+		else if (dst.pixel == GREYA_PIXEL) {
 			png_set_rgb_to_gray(png_ptr, 1, -1.0, -1.0);
 			png_set_expand_gray_1_2_4_to_8(png_ptr);
 			png_set_tRNS_to_alpha(png_ptr);
@@ -198,99 +219,49 @@ namespace picha {
 		png_set_strip_16(png_ptr);
 		png_read_update_info(png_ptr, info_ptr);
 
-		png_uint_32 width = png_get_image_width(png_ptr, info_ptr);
-		png_uint_32 height = png_get_image_height(png_ptr, info_ptr);
-
-		image.alloc(width, height, pixel);
-		rows = new png_bytep[height];
-		for (png_uint_32 y = 0; y < height; ++y)
-			rows[y] = (png_bytep)image.row(y);
+		rows = new png_bytep[dst.height];
+		for (int y = 0; y < dst.height; ++y)
+			rows[y] = (png_bytep)dst.row(y);
 		png_read_image(png_ptr, rows);
 		delete[] rows;
 	}
 
-	bool doPngStat(char * buf, size_t len, int& width, int& height, PixelMode& pixel) {
-		ReadBuffer readbuf;
-		readbuf.srclen = len;
-		readbuf.srcdata = buf;
-		if (png_sig_cmp((png_bytep)readbuf.srcdata, 0, readbuf.srclen < 8 ? readbuf.srclen : 8) != 0)
-			return false;
+	struct PngDecodeCtx {
+		Persistent<Value> dstimage;
+		Persistent<Value> buffer;
+		Persistent<Function> cb;
+		PngReader reader;
 
-		png_infop info_ptr = 0;
-		png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, 0, 0, 0);
-		if (png_ptr == 0)
-			return false;
-
-		PngCtx ctx;
-		png_set_error_fn(png_ptr, &ctx, PngCtx::onError, PngCtx::onWarn);
-		if (setjmp(png_jmpbuf(png_ptr))) {
-			png_destroy_read_struct(&png_ptr, &info_ptr, 0);
-			free(ctx.error);
-			return false;
-		}
-
-		info_ptr = png_create_info_struct(png_ptr);
-		if (info_ptr == 0) {
-			png_destroy_read_struct(&png_ptr, &info_ptr, 0);
-			return false;
-		}
-
-		png_set_read_fn(png_ptr, &readbuf, do_read_record);
-		png_read_info(png_ptr, info_ptr);
-
-		png_byte color = png_get_color_type(png_ptr, info_ptr);
-		if ((color & (PNG_COLOR_MASK_COLOR | PNG_COLOR_MASK_PALETTE)) && (color & PNG_COLOR_MASK_ALPHA))
-			pixel = RGBA_PIXEL;
-		else if ((color & (PNG_COLOR_MASK_COLOR | PNG_COLOR_MASK_PALETTE)))
-			pixel = RGB_PIXEL;
-		else if ((color & PNG_COLOR_MASK_ALPHA))
-			pixel = GREYA_PIXEL;
-		else
-			pixel = GREY_PIXEL;
-
-		width = png_get_image_width(png_ptr, info_ptr);
-		height = png_get_image_height(png_ptr, info_ptr);
-
-		png_destroy_read_struct(&png_ptr, &info_ptr, 0);
-		return true;
-	}
+		NativeImage dst;
+	};
 
 	void UV_decodePNG(uv_work_t* work_req) {
 		PngDecodeCtx *ctx = reinterpret_cast<PngDecodeCtx*>(work_req->data);
-		ctx->doWork();
+		ctx->reader.decode(ctx->dst);
+		ctx->reader.close();
+	}
+
+	void makeCallback(Handle<Function> cb, const char * error, Handle<Value> v) {
+		Local<Value> e;
+		Handle<Value> argv[2] = { Undefined(), v };
+		if (error) {
+			e = Exception::Error(String::New(error));
+			argv[0] = e;
+		}
+
+		TryCatch try_catch;
+
+		cb->Call(Context::GetCurrent()->Global(), 2, argv);
+
+		if (try_catch.HasCaught())
+			FatalException(try_catch);
 	}
 
 	void V8_decodePNG(uv_work_t* work_req, int) {
 		HandleScope scope;
 		PngDecodeCtx *ctx = reinterpret_cast<PngDecodeCtx*>(work_req->data);
-
-		char * error = ctx->error;
-		NativeImage image = ctx->image;
-		Local<Function> cb = Local<Function>::New(ctx->cb);
+		makeCallback(ctx->cb, ctx->reader.error, ctx->dstimage);
 		delete ctx;
-
-		Local<Value> e, r;
-		if (error) {
-			e = Exception::Error(String::New(error));
-			r = *Undefined();
-		}
-		else {
-			e = *Undefined();
-			r = nativeImageToJsImage(image);
-		}
-
-		free(error);
-		image.free();
-
-		TryCatch try_catch;
-
-		Handle<Value> argv[2] = { e, r };
-		cb->Call(Context::GetCurrent()->Global(), 2, argv);
-
-		if (try_catch.HasCaught())
-			FatalException(try_catch);
-
-		return;
 	}
 
 	Handle<Value> decodePng(const Arguments& args) {
@@ -315,11 +286,20 @@ namespace picha {
 		size_t srclen = Buffer::Length(srcbuf);
 
 		PngDecodeCtx * ctx = new PngDecodeCtx;
-		ctx->buffer = Persistent<Object>::New(srcbuf);
+		ctx->reader.open(srcdata, srclen);
+		if (ctx->reader.error) {
+			makeCallback(cb, ctx->reader.error, Undefined());
+			return Undefined();
+		}
+
+		if (pixel == INVALID_PIXEL)
+			pixel = ctx->reader.pixel();
+
+		Local<Object> jsdst = newJsImage(ctx->reader.width(), ctx->reader.height(), pixel);
+		ctx->dstimage = Persistent<Value>::New(jsdst);
+		ctx->buffer = Persistent<Value>::New(srcbuf);
 		ctx->cb = Persistent<Function>::New(cb);
-		ctx->readbuf.srcdata = srcdata;
-		ctx->readbuf.srclen = srclen;
-		ctx->pixel = pixel;
+		ctx->dst = jsImageToNativeImage(jsdst);
 
 		uv_work_t* work_req = new uv_work_t();
 		work_req->data = ctx;
@@ -348,24 +328,26 @@ namespace picha {
 		char* srcdata = Buffer::Data(srcbuf);
 		size_t srclen = Buffer::Length(srcbuf);
 
-		PngDecodeCtx ctx;
-		ctx.readbuf.srcdata = srcdata;
-		ctx.readbuf.srclen = srclen;
-		ctx.pixel = pixel;
-
-		ctx.doWork();
-
-		Local<Value> r = *Undefined();
-		if (ctx.error) {
-			ThrowException(Exception::Error(String::New(ctx.error)));
-			free(ctx.error);
-		}
-		else {
-			r = nativeImageToJsImage(ctx.image);
+		PngReader reader;
+		reader.open(srcdata, srclen);
+		if (reader.error) {
+			ThrowException(Exception::Error(String::New(reader.error)));
+			return Undefined();
 		}
 
-		ctx.image.free();
-		return scope.Close(r);
+		if (pixel == INVALID_PIXEL)
+			pixel = reader.pixel();
+
+		Local<Object> jsdst = newJsImage(reader.width(), reader.height(), pixel);
+
+		reader.decode(jsImageToNativeImage(jsdst));
+
+		if (reader.error) {
+			ThrowException(Exception::Error(String::New(reader.error)));
+			return Undefined();
+		}
+
+		return scope.Close(jsdst);
 	}
 
 	Handle<Value> statPng(const Arguments& args) {
@@ -377,26 +359,27 @@ namespace picha {
 		}
 		Local<Object> srcbuf = args[0]->ToObject();
 
-		PixelMode pixel;
-		int width, height;
-		Local<Value> r = *Undefined();
-		if (doPngStat(Buffer::Data(srcbuf), Buffer::Length(srcbuf), width, height, pixel)) {
-			Local<Object> stat = Object::New();
-			stat->Set(width_symbol, Integer::New(width));
-			stat->Set(height_symbol, Integer::New(height));
-			stat->Set(pixel_symbol, pixelEnumToSymbol(pixel));
-			r = stat;
-		}
+		PngReader reader;
+		reader.open(Buffer::Data(srcbuf), Buffer::Length(srcbuf));
+		if (reader.error)
+			return scope.Close(Undefined());
 
-		return scope.Close(r);
+		Local<Object> stat = Object::New();
+		stat->Set(width_symbol, Integer::New(reader.width()));
+		stat->Set(height_symbol, Integer::New(reader.height()));
+		stat->Set(pixel_symbol, pixelEnumToSymbol(reader.pixel()));
+		return scope.Close(stat);
 	}
 
 
 	//----------------------------------------------------------------------------------------------------------------
 	//--
 
-	struct PngEncodeCtx : PngCtx {
-		PngEncodeCtx() : dstdata(0) {}
+	//----------------------------------------------------------------------------------------------------------------
+	//--
+
+	struct PngEncodeCtx {
+		PngEncodeCtx() : dstdata(0), error(0) {}
 
 		Persistent<Value> buffer;
 		Persistent<Function> cb;
@@ -406,7 +389,17 @@ namespace picha {
 		char *dstdata;
 		size_t dstlen;
 
+		char *error;
+
 		void doWork();
+
+		static void onError(png_structp png_ptr, png_const_charp error) {
+			PngEncodeCtx * self = (PngEncodeCtx*)png_get_error_ptr(png_ptr);
+			self->error = strdup(error);
+			longjmp(png_jmpbuf(png_ptr), 1);
+		}
+
+		static void onWarn(png_structp png_ptr, png_const_charp error) {}
 	};
 
 	int pixelToPngMode(PixelMode p) {
@@ -433,7 +426,7 @@ namespace picha {
 		}
 
 		WriteBuffer *writebuf = new WriteBuffer;
-		png_set_error_fn(png_ptr, this, PngCtx::onError, PngCtx::onWarn);
+		png_set_error_fn(png_ptr, this, PngEncodeCtx::onError, PngEncodeCtx::onWarn);
 		if (setjmp(png_jmpbuf(png_ptr))) {
 			png_destroy_write_struct(&png_ptr, &info_ptr);
 			delete writebuf;
