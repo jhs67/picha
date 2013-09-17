a716322e55f094a2d69ead215c359465624aa68c
Jon Spencer <jon@jonspencer.ca>
2013-09-16 22:26:57 -0700

    copy pixels between images

---
+++ test/copy.js
@@ -0,0 +1,17 @@
+
+var fs = require('fs');
+var path = require('path');
+var assert = require('assert');
+var picha = require('../index.js');
+
+describe('copy', function() {
+	var image;
+	it("should load test image", function() {
+		image = picha.decodeSync(fs.readFileSync(path.join(__dirname, "test2.jpg")));
+	});
+	it("should copy pixels", function() {
+		var c = new picha.Image({ width: 30, height: 30, pixel: image.pixel });
+		image.copy(c);
+		assert(image.subView(0, 0, 30, 30).equalPixels(c));
+	})
+})
